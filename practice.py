3665f360c3dc003e474a4713f0d3ce21da8abb6f
Zohaib Khan <zohaibk1111@gmail.com>
2022-06-13 01:29:25 -0400

    test

--- practice.py
+++ practice.py
@@ -1,2 +1,18 @@
 score = 22
-print(f"your score is {score}")
+print(f"You have {days}")
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+diff_of_age = 90 - int(age)
+years = diff_of_age
+months = diff_of_age * 12
+weeks = diff_of_age * 52
+days = diff_of_age * 365
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left ")
+
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+
